e1981bf0ffc5f3d44ad7e4a18cff6320ef453df6
David Grayson <davidegrayson@gmail.com>
2020-06-09 20:36:21 -0700

    libdvdread: 6.0.1 -> 6.0.2

--- pkgs/libdvdread/default.nix
+++ pkgs/libdvdread/default.nix
@@ -3,11 +3,11 @@
 crossenv.make_derivation rec {
   name = "libdvdread-${version}";
 
-  version = "6.0.1";
+  version = "6.0.2";
 
   src = crossenv.nixpkgs.fetchurl {
     url = "https://download.videolan.org/pub/videolan/libdvdread/${version}/libdvdread-${version}.tar.bz2";
-    sha256 = "1gfmh8ii3s2fw1c8vn57piwxc0smd3va4h7xgp9s8g48cc04zki8";
+    sha256 = "1c7yqqn67m3y3n7nfrgrnzz034zjaw5caijbwbfrq89v46ph257r";
   };
 
   builder = ./builder.sh;
