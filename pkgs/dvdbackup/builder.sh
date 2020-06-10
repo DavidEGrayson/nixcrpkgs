4e48a191e378d7342793d6d4a9ce6783e39a455d
David Grayson <davidegrayson@gmail.com>
2020-06-09 21:00:11 -0700

    Progress on compiling win32.dvdbackup.

--- pkgs/dvdbackup/builder.sh
+++ pkgs/dvdbackup/builder.sh
@@ -4,8 +4,21 @@ tar -xf $src
 
 mv dvdbackup-* dvdbackup
 
+cd dvdbackup
+for patch in $patches; do
+  echo applying patch $patch
+  patch -p1 -i $patch
+done
+cd ..
+
+
 mkdir build
 cd build
-../dvdbackup/configure --host=$host  --prefix=$out
 
-# TODO
+export CFLAGS="$(pkg-config-cross --cflags dvdread) -Wno-unused-value -Wno-unused-parameter"
+export LDFLAGS="$(pkg-config-cross --libs dvdread)"
+../dvdbackup/configure --host=$host --prefix=$out
+
+make
+
+make install
--- pkgs/dvdbackup/default.nix
+++ pkgs/dvdbackup/default.nix
@@ -10,6 +10,8 @@ crossenv.make_derivation rec {
     sha256 = "1rl3h7waqja8blmbpmwy01q9fgr5r0c32b8dy3pbf59bp3xmd37g";
   };
 
+  patches = [ ./mingw.patch ];
+
   builder = ./builder.sh;
 
   cross_inputs = [ libdvdread ];
