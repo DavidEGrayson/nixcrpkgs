bcba48f7fb836b271ac15aeab70a0733b3262fa0
David Grayson <davidegrayson@gmail.com>
2025-01-14 14:24:53 -0800

    Update binutils to 2.43 and add a patch to make 32-bit executables run on the Raspberry Pi 5 again.
    This fixes a problem introduced in binutils 2.40 by commit fedaf87c.
    I had to do a lot of bisecting to figure this out.

--- native/binutils/default.nix
+++ native/binutils/default.nix
@@ -5,15 +5,18 @@ in
 env.make_derivation rec {
   name = "binutils-${version}-${host}";
 
-  version = "2.39";
-
+  version = "2.43";
   src = env.nixpkgs.fetchurl {
     url = "mirror://gnu/binutils/binutils-${version}.tar.xz";
-    hash = "sha256-ZFwl9WO4rcCoHb1qQc/79NNwg6OC4C1dPfT2XAlRbQA=";
+    hash = "sha256-tTYG9EOsjwHR1fycOUl/KvMi2Z4UzqXAtLEk1jA3k2U=";
   };
 
   patches = [
     ./deterministic.patch
+
+    # The Raspberry Pi 5 kernel uses 16 KiB pages.  Without this patch, 32-bit
+    # executables fail at startup time on RPi5 with a segmentation fault.
+    ./arm_page_size.patch
   ];
 
   native_inputs = [ nixpkgs.bison nixpkgs.flex nixpkgs.m4 nixpkgs.texinfo ];
